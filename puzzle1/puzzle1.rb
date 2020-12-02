f4f294973380698c567d44fa530e1b7938baf88f
Arenui Anderson <arenui.anderson@gmail.com>
2020-12-02 09:23:51 -0700

    added second challenge to first puzzle

--- puzzle1/puzzle1.rb
+++ puzzle1/puzzle1.rb
@@ -1,9 +1,19 @@
 # Puzzle 1 for Advent of Code
 
-numbers = File.read("input-puzzle1.txt").split.map(&:to_i)
+numbers = File.read("puzzle1/input-puzzle1.txt").split.map(&:to_i)
 
 numbers.each do |num|
   if numbers.include? (2020 - num)
+    puts "I ain't never seen two pretty numbers:"
     puts num * (2020 - num)
+  end
+end
+
+numbers.each do |num1|
+  numbers.each do |num2|
+    if numbers.include? (2020 - num1 - num2)
+      puts "Three's a crowd:"
+      puts num1 * num2 * (2020 - num1 - num2)
+    end
   end
 end
